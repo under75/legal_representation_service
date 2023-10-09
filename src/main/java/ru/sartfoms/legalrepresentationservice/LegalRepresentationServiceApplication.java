1e13006f139dc3441c2c78b76285032fe4ca8d2e
pylaev@sartfoms.ru <pylaev@sartfoms.ru>
2023-10-09 17:53:40 +0400

    snapshot

---
+++ src/main/java/ru/sartfoms/legalrepresentationservice/LegalRepresentationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.sartfoms.legalrepresentationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalRepresentationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalRepresentationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sartfoms/legalrepresentationservice/LegalRepresentationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sartfoms.legalrepresentationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalRepresentationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
